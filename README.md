a683702e7cfa5cc37b0769ea6fb369f44f2b87c5
cowlik <clint@cowlik.com>
2017-01-16 21:22:52 -0800

    added NPM/Gulp instructions; setup CSS

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 #Tease
 
+### 1. Install global dependencies:
+
+> [Node.js](https://nodejs.org/en/download/)
+
+> Harp: `npm install -g harp`
+
+> Gulp: `npm install -g gulp-cli`
+
+### 2. Install project dependencies:
+
+> `cd "C:\Users\..."` 
+
+> `npm install --only=dev`
+
+### 3. Run Harp server:
+
+> `harp server` *or* `gulp test`
+
+> Open browser and type `localhost:9000`
+
+### 4. Compile Harp site:
+
+> `gulp` *or* `gulp build`
+
+---
+
+#### Resources:
+
+[Harp](http://harpjs.com)
+
+[Jade](http://jade-lang.com/)
+
+[SASS](http://sass-lang.com/)
+
+[Markdown](https://daringfireball.net/projects/markdown/)
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+﻿var gulp = require("gulp"),
+    cssNano = require("cssnano"),
+    del = require("del"),
+    postCSS = require("gulp-postcss"),
+    rename = require("gulp-rename"),
+    shell = require("gulp-shell"),
+    uglify = require("gulp-uglify"),
+    vinylPaths = require("vinyl-paths");
+
+gulp.task("minify:css", function() {
+    var processors = [cssNano({ autoprefixer: false, discardUnused: false, zindex: false })];
+    return gulp.src("www/css/*.css")
+        .pipe(vinylPaths(del))
+        .pipe(postCSS(processors))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("www/css"))
+});
+
+gulp.task("minify:js", function() {
+    return gulp.src("www/js/*.js")
+        .pipe(vinylPaths(del))
+        .pipe(uglify())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("www/js"))
+});
+
+gulp.task("minify", ["minify:css", "minify:js"]);
+
+gulp.task("test", shell.task([
+    "harp server"
+]));
+
+gulp.task("build", shell.task([
+    "harp compile",
+    "gulp minify"
+]));
+
+gulp.task("default", ["build"]);
