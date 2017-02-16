1c5fe56f493d8bd49efab2a2d0ccdc19bd86b01f
cowlik <clint@cowlik.com>
2017-02-15 18:41:22 -0800

    experimenting w/ Booking plugin

---
+++ public/js/booking.js
@@ -0,0 +1,18 @@
+var Tease = Tease || {};
+
+/*
+ *
+ * booking
+ *
+ */
+
+Tease.Booking = {
+    init: function() {
+        var booking = this;
+    }
+};
+
+$(function() {
+    Tease.Booking.init();
+});
+
